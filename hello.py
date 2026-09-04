9fd2ca669e832b934413f4efe80d70cc9d3d4c9e
Alister <Alister_email@example.com>
2026-09-04 11:17:07 +0800

    follower count

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("==================================")
 print("Welcome")
 print("It's my first post")
 print("==================================")
+
+username = "Alister"
+bio = "Testing thissss"
+follower = 2026
+
+print("Username:", username)
+print("Bio: ", bio)
+print("Followers: ", follower)
